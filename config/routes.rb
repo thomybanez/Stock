5a693ff4525823226894e7e09e233a7a74b74ac3
thomybanez <thomybanez@yahoo.com>
2023-04-11 21:36:14 +0800

    feature: fix previous user controller,

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do 
   root "users#home"
-  get 'home', to: 'users#home', as: 'home'
-  get 'signup', to: 'users#signup', as: 'signup'
-  post 'signup', to: 'users#create', as: 'create'
+
+
+  #Sessions
+  get '/signin', to: 'sessions#signin', as: 'sessions_signin'
+  post '/signin/authenticate', to: 'sessions#signin_authenticate', as: 'sessions_signin_authenticate'
+
+  get '/signup', to: 'sessions#signup', as: 'sessions_signup'
+  post '/signup/submit', to: 'sessions#signup_submit', as: 'sessions_signup_submit'
+
+
 end
