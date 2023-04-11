946845be3a1bd54e2fc895aa457f4b4577a16ea8
thomybanez <thomybanez@yahoo.com>
2023-04-11 18:55:05 +0800

    feature:signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do 
   root "users#home"
-  get '/home', to: 'users#home', as: 'home'
-  get '/signup', to: 'users#signup', as: 'signup'
-  post '/create', to: 'users#create', as: 'create'
-
+  get 'home', to: 'users#home', as: 'home'
+  get 'signup', to: 'users#signup', as: 'signup'
+  post 'signup', to: 'users#create', as: 'create'
 end
