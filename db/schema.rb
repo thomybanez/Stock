e96f77eabed670a281582f0320957984127844f5
thomybanez <thomybanez@yahoo.com>
2023-04-18 16:14:32 +0800

    feature: updated home_path location to users_home_path

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_04_13_121459) do
   create_table "trades", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_030105) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
