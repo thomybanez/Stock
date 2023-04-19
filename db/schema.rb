2100031b4c97a7d2b2180a2597d600b27924a6eb
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-04-19 11:34:43 +0800

    updated trade migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_121459) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_033244) do
   create_table "trades", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "entry_price"
+    t.decimal "exit_price"
+    t.string "coin_id"
+    t.decimal "size", default: "0.0"
+    t.boolean "is_ongoing", default: true
   end
 
   create_table "users", force: :cascade do |t|
