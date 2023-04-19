3be3f23cba2803db9c0e332c3c3f15f768ee055c
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-04-19 18:32:20 +0800

    added trade page render and buy functionality

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,6 @@
 class TradesController < ApplicationController
     before_action :set_user
+    before_action :set_position, only: [:execute]
     
     def market
         @api = CoinGecko::Client.retrieve_coin[:data]     
@@ -7,17 +8,37 @@ class TradesController < ApplicationController
 
     def trade
         @coin_id = params[:coin_id]
-        @entry_price = params[:coin_price]
-        @trade = Trade.new
+        @entry_price = params[:entry_price]
         @size = params[:size]
+        @trade = Trade.new
     end
 
     def execute
         @trade = Trade.new(trade_params)
-        @trade.save
+        @trade.user_id = session[:user_id]
+
+        @position = Position.find_by(coin_id: @trade.coin_id)
+        puts @position
+        if @position && @position.quantity > 0
+            puts "spa"
+            @trade.position_id = @position.id
+        else
+            puts "spa2"
+            new_position = Position.create(quantity: @trade.size, coin_id: @trade.coin_id, average_entry: @trade.entry_price, user_id: session[:user_id])
+            @trade.position_id = new_position.id
+        end
+
+        if @trade.save
+            redirect_to trade_path(coin_id: @trade.coin_id, entry_price: @trade.entry_price)
+        else
+            redirect_to trade_path(coin_id: @trade.coin_id, entry_price: @trade.entry_price)
+        end
     end
 
     private
+    def set_position
+    end
+
     def set_user
         user_id = session[:user_id]
         if user_id
