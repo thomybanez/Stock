6d3da0199d4593c74eeebe40be548a8bf32e33c5
Thom Ybanez <thomybanez@yahoo.com>
2023-06-08 17:37:14 +0800

    Render Display

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Trade, type: :model do
    
    describe "User-Trade association" do
     it "associates a trade with a user" do
-      user = build(:user) # build a new user record
+      user = User.create(email: "john@yahoo.com") # build a new user record
       trade = Trade.create(entry_price: 1, coin_id: 1, size: 1.1, user_id: user.id)
 
-      expect(trade.user).to eq(user)
+      expect(trade.user_id).to eq(user.id)
     end
   end
 end
