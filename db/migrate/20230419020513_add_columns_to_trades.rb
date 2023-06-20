7c076c847c26f8b07b1a5df3c7c90601897584ab
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-06-20 14:37:22 +0800

    setup render deploy

--- db/migrate/20230419020513_add_columns_to_trades.rb
+++ db/migrate/20230419020513_add_columns_to_trades.rb
@@ -1,8 +1,8 @@
 class AddColumnsToTrades < ActiveRecord::Migration[7.0]
   def change
-    add_column :trades, :entry_price, :decimal
-    add_column :trades, :exit_price, :decimal
-    add_column :trades, :coin_id, :string
-    add_column :trades, :size, :decimal
+    change_column :trades, :entry_price, :decimal
+    change_column :trades, :exit_price, :decimal
+    change_column :trades, :coin_id, :string
+    change_column :trades, :size, :decimal
   end
 end
